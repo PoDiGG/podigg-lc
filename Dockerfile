8065cd7067bb955095b4cd2e714a0a90169b50a8
Ruben Taelman <ruben.taelman@ugent.be>
2016-12-15 15:03:19 +0100

    Fix incorrect bin invocation in Docker container

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,7 @@ FROM node:6.7.0
 
 ENV NODE_MEM 2056
 
-ADD podigg podigg
 ADD bin bin
 ADD package.json package.json
-RUN cd podigg && npm install && npm link
-RUN npm link podigg && npm install
-CMD node --max_old_space_size=${NODE_MEM} bin/generate.js /tmp/output_data
+RUN npm install
+CMD node --max_old_space_size=${NODE_MEM} bin/generate-env /tmp/output_data
--- bin/generate.js
+++ bin/generate.js
@@ -1,25 +1,28 @@
-#!/usr/bin/env node
 'use strict';
 
 const childProcess = require('child_process');
 const fs = require('fs');
 
-var path = process.argv.length < 3 ? '.' : process.argv[2];
+module.exports = function(type) {
+    process.argv.shift();
+    process.argv.shift();
+    var path = process.argv.length < 1 ? '.' : process.argv[0];
 
-var process_generate = childProcess.spawn('./node_modules/podigg/bin/generate-env.js', [path]);
-process_generate.stdout.pipe(process.stdout);
-process_generate.stderr.pipe(process.stderr);
-process_generate.on('close', (code_generate) => {
-    if(!code_generate) {
-        var process_convert = childProcess.spawn('./node_modules/gtfs2lc/bin/gtfs2lc.js', [path + '/gtfs', '-f', 'turtle', '-r', '-t', '-a']);
-        process_convert.stdout.pipe(fs.createWriteStream(path + '/lc.ttl'));
-        process_convert.stderr.pipe(process.stderr);
-        process_convert.on('close', (code_convert) => {
-            if (code_generate) {
-                throw new Error('Conversion exited with exit code ' + code_convert);
-            }
-        });
-    } else {
-        throw new Error('Generation exited with exit code ' + code_generate);
-    }
-});
+    var process_generate = childProcess.spawn('./node_modules/podigg/bin/generate-' + type + '.js', process.argv);
+    process_generate.stdout.pipe(process.stdout);
+    process_generate.stderr.pipe(process.stderr);
+    process_generate.on('close', (code_generate) => {
+        if(!code_generate) {
+            var process_convert = childProcess.spawn('./node_modules/gtfs2lc/bin/gtfs2lc.js', [path + '/gtfs', '-f', 'turtle', '-r', '-t', '-a']);
+            process_convert.stdout.pipe(fs.createWriteStream(path + '/lc.ttl'));
+            process_convert.stderr.pipe(process.stderr);
+            process_convert.on('close', (code_convert) => {
+                if (code_generate) {
+                    throw new Error('Conversion exited with exit code ' + code_convert);
+                }
+            });
+        } else {
+            throw new Error('Generation exited with exit code ' + code_generate);
+        }
+    });
+}
